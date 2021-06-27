84473458029570104587faa43431dce9c402b4f7
Sushil Kumar Gupta <68986746+skgupta77159@users.noreply.github.com>
2021-06-27 15:34:24 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+import Footer from './Footer';
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>Javascript and React.js</h1>
+            <p>A basic web dev react Js Bootcamp</p>   
+        </div>
+    )
+}
+
+export default Note;
